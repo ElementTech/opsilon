37919f01bc4fa80d309e06f0fb469c875b4fd5be
Amitai Getzler <amitai.getzler@explorium.ai>
2022-11-14 18:51:25 +0200

    :art: update
    modified:   internal/engine/engine.go; modified:   internal/validate/validate.go

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -28,31 +28,31 @@ import (
 )
 
 type Input struct {
-	Name     string `mapstructure:"name" validate:"nonzero"`
+	Name     string `mapstructure:"name" validate:"nonzero,nowhitespace"`
 	Default  string `mapstructure:"default"`
 	Optional bool   `mapstructure:"optional,omitempty"`
 }
 
 type Stage struct {
 	Stage     string   `mapstructure:"stage" validate:"nonzero"`
-	ID        string   `mapstructure:"id,omitempty" validate:"nonzero"`
+	ID        string   `mapstructure:"id,omitempty" validate:"nonzero,nowhitespace"`
 	Script    []string `mapstructure:"script" validate:"nonzero"`
 	If        string   `mapstructure:"if,omitempty"`
 	Clean     bool     `mapstructure:"clean,omitempty"`
 	Env       []Env    `mapstructure:"env,omitempty"`
 	Artifacts []string `mapstructure:"artifacts,omitempty"`
 	Image     string   `mapstructure:"image,omitempty"`
-	Needs     string   `mapstructure:"needs,omitempty"`
+	Needs     string   `mapstructure:"needs,omitempty" validate:"nowhitespace"`
 }
 
 type Env struct {
-	Name  string `mapstructure:"name" validate:"nonzero"`
+	Name  string `mapstructure:"name" validate:"nonzero,nowhitespace"`
 	Value string `mapstructure:"value" validate:"nonzero"`
 }
 
 type Workflow struct {
-	ID          string  `mapstructure:"id" validate:"nonzero"`
-	Image       string  `mapstructure:"image" validate:"nonzero"`
+	ID          string  `mapstructure:"id" validate:"nonzero,nowhitespace"`
+	Image       string  `mapstructure:"image" validate:"nonzero,nowhitespace"`
 	Description string  `mapstructure:"description"`
 	Env         []Env   `mapstructure:"env"`
 	Input       []Input `mapstructure:"input"`
--- internal/validate/validate.go
+++ internal/validate/validate.go
@@ -1,14 +1,29 @@
 package validate
 
 import (
+	"errors"
 	"os"
+	"reflect"
+	"strings"
 
 	"github.com/jatalocks/opsilon/internal/config"
 	"github.com/jatalocks/opsilon/internal/logger"
 	"gopkg.in/validator.v2"
 )
 
+func noWhiteSpace(v interface{}, param string) error {
+	st := reflect.ValueOf(v)
+	if st.Kind() != reflect.String {
+		return errors.New("noWhiteSpace only validates strings")
+	}
+	if strings.Contains(st.String(), " ") {
+		return errors.New("value cannot contain spaces")
+	}
+	return nil
+}
+
 func ValidatePopulatedActionFile(a *[]config.Action) {
+	validator.SetValidationFunc("nowhitespace", noWhiteSpace)
 	if errs := validator.Validate(&a); errs != nil {
 		logger.Operation("Your Configuration has Problems:")
 		logger.Error(errs.Error())
