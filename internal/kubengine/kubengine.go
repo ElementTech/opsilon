988d8225817a97d7406b0df6cfab1d0c1742bf94
Amitai Getzler <amitai.getzler@explorium.ai>
2022-11-19 19:40:52 +0200

    :sparkles: init container
    modified:   examples/workflows/example-full.ops.yaml; modified:   go.mod; modified:   go.sum; modified:   internal/kubengine/kubengine.go; new file:   internal/kubengine/pod/podexec.go; new file:   internal/kubengine/pod/podfile.go

--- internal/kubengine/kubengine.go
+++ internal/kubengine/kubengine.go
@@ -1,9 +1,14 @@
 package kubengine
 
 import (
+	"archive/tar"
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"os"
+	"path"
+	"path/filepath"
 	"regexp"
 	"strings"
 	"sync"
@@ -17,14 +22,18 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/remotecommand"
+	"k8s.io/kubectl/pkg/scheme"
 )
 
 type Client struct {
-	k8s kubernetes.Interface
-	ns  string
+	k8s    kubernetes.Interface
+	ns     string
+	config rest.Config
 }
 
 type RequestBody struct {
@@ -82,7 +91,7 @@ func (c *Client) RemoveVolume(ctx context.Context, vol string, claim *v1.Persist
 	c.k8s.CoreV1().PersistentVolumes().Delete(ctx, vol, deleteOptions)
 }
 
-func (c *Client) CreatePod(ctx context.Context, name string, image string, command []string, envs []engine.Env, volume string, claim *v1.PersistentVolumeClaim, volumeOutput string, claimOutput *v1.PersistentVolumeClaim) error {
+func (c *Client) CreatePod(ctx context.Context, name string, image string, command []string, envs []engine.Env, volume string, claim *v1.PersistentVolumeClaim, volumeOutput string, claimOutput *v1.PersistentVolumeClaim) (error, v1.Pod) {
 	envVar := ToV1Env(envs)
 	pod := &v1.Pod{
 		ObjectMeta: metav1.ObjectMeta{Name: name},
@@ -95,13 +104,22 @@ func (c *Client) CreatePod(ctx context.Context, name string, image string, comma
 				Name:         volumeOutput,
 				VolumeSource: v1.VolumeSource{PersistentVolumeClaim: &v1.PersistentVolumeClaimVolumeSource{ClaimName: claimOutput.Name}},
 			}},
-			Containers: []v1.Container{
+			InitContainers: []v1.Container{
 				v1.Container{
 					Name:         "main",
 					Image:        image,
 					Command:      command,
 					WorkingDir:   "/app",
 					Env:          *envVar,
+					VolumeMounts: []v1.VolumeMount{{Name: volume, MountPath: "/app"}, {Name: volumeOutput, MountPath: "/output", SubPath: "output"}},
+				},
+			},
+			Containers: []v1.Container{
+				v1.Container{
+					Name:         "keepalive",
+					Image:        "busybox",
+					Command:      []string{"/bin/sh", "-c", "sleep 60"},
+					WorkingDir:   "/app",
 					VolumeMounts: []v1.VolumeMount{{Name: volume, MountPath: "/app"}, {Name: volumeOutput, MountPath: "/output", SubPath: "output"}},
 				},
 			},
@@ -112,7 +130,7 @@ func (c *Client) CreatePod(ctx context.Context, name string, image string, comma
 		Pods(c.ns).
 		Create(ctx, pod, metav1.CreateOptions{})
 
-	return err
+	return err, *pod
 }
 
 func (c *Client) GetPodExitCode(ctx context.Context, name string) (int32, error) {
@@ -186,7 +204,7 @@ func NewClient() (*Client, error) {
 		},
 	}
 	k8s.CoreV1().Namespaces().Create(context.Background(), ns, metav1.CreateOptions{})
-	return &Client{k8s: k8s, ns: ns.Name}, nil
+	return &Client{k8s: k8s, ns: ns.Name, config: *config}, nil
 }
 func (cli *Client) KubeEngine(wg *sync.WaitGroup, sID string, ctx context.Context, w engine.Workflow, vol string, claim *v1.PersistentVolumeClaim, allOutputs map[string][]engine.Env, skippedStages *[]string) {
 	defer wg.Done()
@@ -265,6 +283,78 @@ func clearString(str string) string {
 	return nonAlphanumericRegex.ReplaceAllString(str, "")
 }
 
+func (c *Client) createPodWatcher(ctx context.Context, resName string) (watch.Interface, error) {
+	fieldSelector := fmt.Sprintf("metadata.name=%s", resName)
+
+	opts := metav1.ListOptions{
+		TypeMeta:      metav1.TypeMeta{},
+		FieldSelector: fieldSelector,
+	}
+
+	return c.k8s.CoreV1().Pods(c.ns).Watch(ctx, opts)
+}
+
+func (c *Client) waitPodRunning(ctx context.Context, resName string, LwWhite *logger.MyLogWriter) error {
+	watcher, err := c.createPodWatcher(ctx, resName)
+	if err != nil {
+		return err
+	}
+
+	defer watcher.Stop()
+
+	for {
+		select {
+		case event := <-watcher.ResultChan():
+			pod := event.Object.(*v1.Pod)
+
+			if pod.Status.Phase == v1.PodRunning {
+				// LwWhite.Write([]byte(fmt.Sprintf("The POD \"%s\" is running/success\n", resName)))
+				return nil
+			}
+
+		case <-ctx.Done():
+			// LwWhite.Write([]byte(fmt.Sprintf("Exit from waitPodRunning for POD \"%s\" because the context is done", resName)))
+			return nil
+		}
+	}
+}
+
+func (c *Client) getPodLogs(ctx context.Context, podName string, LwWhite *logger.MyLogWriter) error {
+	count := int64(100)
+	podLogOptions := v1.PodLogOptions{
+		Follow:    true,
+		TailLines: &count,
+	}
+
+	err := c.waitPodRunning(ctx, podName, LwWhite)
+	logger.HandleErr(err)
+
+	podLogRequest := c.k8s.CoreV1().
+		Pods(c.ns).
+		GetLogs(podName, &podLogOptions)
+	stream, err := podLogRequest.Stream(ctx)
+	if err != nil {
+		return err
+	}
+	defer stream.Close()
+	for {
+		buf := make([]byte, 2000)
+		numBytes, err := stream.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		if numBytes == 0 {
+			continue
+		}
+		if err != nil {
+			return err
+		}
+		message := string(buf[:numBytes])
+		LwWhite.Write([]byte(message))
+	}
+	return nil
+}
+
 func (cli *Client) RunStageKubernetes(s engine.Stage, ctx context.Context, envs []engine.Env, globalImage string, volume string, claim *v1.PersistentVolumeClaim, volumeOutput string, claimOutput *v1.PersistentVolumeClaim, LwWhite *logger.MyLogWriter) {
 	LwWhite.Write([]byte(fmt.Sprintf("Running Stage with the following variables: %s\n", engine.GenEnv(envs))))
 	envs = append(envs, []engine.Env{{Name: "OUTPUT", Value: "/output/output"}}...)
@@ -280,7 +370,7 @@ func (cli *Client) RunStageKubernetes(s engine.Stage, ctx context.Context, envs 
 	// 	Tty:        false,
 	// }, &hostConfig, nil, nil, "")
 	podName := strings.ReplaceAll(clearString(fmt.Sprint(s.Stage+"-"+s.ID)), " ", "-")
-	err := cli.CreatePod(
+	err, pod := cli.CreatePod(
 		ctx,
 		podName,
 		globalImage,
@@ -292,16 +382,146 @@ func (cli *Client) RunStageKubernetes(s engine.Stage, ctx context.Context, envs 
 		claimOutput,
 	)
 	logger.HandleErr(err)
-	exitCode, err := cli.GetPodExitCode(ctx, podName)
+	// exitCode, err := cli.GetPodExitCode(ctx, podName)
+	// logger.HandleErr(err)
+	err = cli.getPodLogs(ctx, podName, LwWhite)
 	logger.HandleErr(err)
+
+	wd, err := os.Getwd()
 	logger.HandleErr(err)
-	stdout, err := cli.GetPodStdOut(ctx, podName)
+	err = copyFromPod(cli, "/output/output", wd+"/output", pod)
 	logger.HandleErr(err)
+
 	go func() {
 		if err := cli.DeletePod(ctx, podName); err != nil {
 			log.Printf("Error deleting pod: %v", err)
 		}
 	}()
-	logger.Info(fmt.Sprint(exitCode))
-	logger.Free(stdout)
+	// logger.Info(fmt.Sprint(exitCode))
+}
+func getPrefix(file string) string {
+	return strings.TrimLeft(file, "/")
+}
+func copyFromPod(cli *Client, srcPath string, destPath string, i v1.Pod) error {
+	restconfig := cli.config
+	reader, outStream := io.Pipe()
+	//todo some containers failed : tar: Refusing to write archive contents to terminal (missing -f option?) when execute `tar cf -` in container
+	cmdArr := []string{"tar", "cf", "-", srcPath}
+	req := cli.k8s.CoreV1().RESTClient().Get().
+		Namespace(cli.ns).
+		Resource("pods").
+		Name(i.Name).
+		SubResource("exec").
+		VersionedParams(&v1.PodExecOptions{
+			Container: i.Spec.Containers[0].Name,
+			Command:   cmdArr,
+			Stdin:     true,
+			Stdout:    true,
+			Stderr:    true,
+			TTY:       false,
+		}, scheme.ParameterCodec)
+	exec, err := remotecommand.NewSPDYExecutor(&restconfig, "POST", req.URL())
+	if err != nil {
+		log.Fatalf("error %s\n", err)
+		return err
+	}
+	go func() {
+		defer outStream.Close()
+		err = exec.Stream(remotecommand.StreamOptions{
+			Stdin:  os.Stdin,
+			Stdout: outStream,
+			Stderr: os.Stderr,
+			Tty:    false,
+		})
+		logger.HandleErr(err)
+	}()
+	prefix := getPrefix(srcPath)
+	prefix = path.Clean(prefix)
+	prefix = stripPathShortcuts(prefix)
+	destPath = path.Join(destPath, path.Base(prefix))
+	err = untarAll(reader, destPath, prefix)
+	return err
+}
+
+func stripPathShortcuts(p string) string {
+	newPath := p
+	trimmed := strings.TrimPrefix(newPath, "../")
+
+	for trimmed != newPath {
+		newPath = trimmed
+		trimmed = strings.TrimPrefix(newPath, "../")
+	}
+
+	// trim leftover {".", ".."}
+	if newPath == "." || newPath == ".." {
+		newPath = ""
+	}
+
+	if len(newPath) > 0 && string(newPath[0]) == "/" {
+		return newPath[1:]
+	}
+
+	return newPath
+}
+
+func untarAll(reader io.Reader, destDir, prefix string) error {
+	tarReader := tar.NewReader(reader)
+	for {
+		header, err := tarReader.Next()
+		if err != nil {
+			if err != io.EOF {
+				return err
+			}
+			break
+		}
+
+		if !strings.HasPrefix(header.Name, prefix) {
+			return fmt.Errorf("tar contents corrupted")
+		}
+
+		mode := header.FileInfo().Mode()
+		destFileName := filepath.Join(destDir, header.Name[len(prefix):])
+
+		baseName := filepath.Dir(destFileName)
+		if err := os.MkdirAll(baseName, 0755); err != nil {
+			return err
+		}
+		if header.FileInfo().IsDir() {
+			if err := os.MkdirAll(destFileName, 0755); err != nil {
+				return err
+			}
+			continue
+		}
+
+		evaledPath, err := filepath.EvalSymlinks(baseName)
+		if err != nil {
+			return err
+		}
+
+		if mode&os.ModeSymlink != 0 {
+			linkname := header.Linkname
+
+			if !filepath.IsAbs(linkname) {
+				_ = filepath.Join(evaledPath, linkname)
+			}
+
+			if err := os.Symlink(linkname, destFileName); err != nil {
+				return err
+			}
+		} else {
+			outFile, err := os.Create(destFileName)
+			if err != nil {
+				return err
+			}
+			defer outFile.Close()
+			if _, err := io.Copy(outFile, tarReader); err != nil {
+				return err
+			}
+			if err := outFile.Close(); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
